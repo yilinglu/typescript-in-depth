8178b4c851490beb9aef252cea4e60e3bd8e9037
Yiling Lu <yilingl@gmail.com>
2016-08-02 14:45:21 -0700

    Add typings tool to bring in .d.ts files for lodash.

--- LibraryManager/app.ts
+++ LibraryManager/app.ts
@@ -1,19 +1,28 @@
-function GetAllBooks(){
+import * as lo from "lodash";
+
+function GetAllBooks() {
   let books = [
-		{ title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
-		{ title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
-		{ title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
-		{ title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
+    { id: 1, title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+    { id: 2, title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+    { id: 3, title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+    { id: 4, title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
   ]
   return books;
 }
 
+let x = "From Here and Forward, and more.";
+let y = lo.snakeCase(x);
+let z = lo.camelCase(y);
+console.log('snake cased: ' + y);
+console.log('camel cased: ' + z);
+
+
 function LogFirstAvailable(books): void {
   let numberOfBooks: number = books.length;
 
-    let firstAvailable:string = '';
-  for(let currentBook of books){
-    if(currentBook.available){
+  let firstAvailable: string = '';
+  for (let currentBook of books) {
+    if (currentBook.available) {
       firstAvailable = currentBook.title;
       break;
     }
@@ -24,12 +33,12 @@ function LogFirstAvailable(books): void {
 }
 
 function getTitleByCategory(catFilter: Category): Array<string> {
-  console.log('Filtering by category: ' + catFilter);
+  console.log('Filtering by category: ' + Category[catFilter]);
   const allBooks = GetAllBooks();
-  const filteredTitles :string[] = [];
+  const filteredTitles: string[] = [];
 
-  for(let book of allBooks){
-    if(book.category === catFilter){
+  for (let book of allBooks) {
+    if (book.category === catFilter) {
       filteredTitles.unshift(book.title);
     }
   }
@@ -40,13 +49,52 @@ function getTitleByCategory(catFilter: Category): Array<string> {
   return filteredTitles;
 }
 
-enum Category {Biography, Fiction, Poetry};
+//function getBookById
+// ts platform will assign 0 based index to each string value
+enum Category { Biography, Fiction, Poetry };
+
+
+enum BigCategory { Biography = 1, Fiction, Poetry = 9 }; // 1, 2, 9
+
+
+enum Category3 { Biography = 1, Fiction, Poetry }; // 1,2,3
+
 
 const allBooks = GetAllBooks();
 LogFirstAvailable(allBooks);
 
-function LogBookTitles(titles: string[]){
-  titles.forEach(()=>{
-
+function LogBookTitles(titles: string[]) {
+  titles.forEach((title) => {
+    console.log('Book Title Listing: ' + title + '.');
   });
+
 }
+
+function logCategory(cat: BigCategory) {
+  console.log('-- Index of fiction: ' + cat);
+}
+
+logCategory(BigCategory.Fiction);
+
+//*********************************************************
+LogBookTitles(getTitleByCategory(Category.Poetry));
+
+const fictionalBooks = getTitleByCategory(Category.Fiction);
+
+fictionalBooks.forEach(
+  // val: current element, idx: index of the current element, arr current array
+  (val, idx, arr)=>{
+    console.log('>>>-- For each print: idx - ' + idx + ', value - ' + val);
+  }
+);
+
+// ways to declare arrays
+
+let strArray1: string[] = ['hear', 'now', 'then'];
+let strArray2: Array<string> = ['hear', 'now', 'then'];
+let anyArray: any[] = ['hear', 'now', 'then'];
+
+// tuples
+
+let myTuple: [number, string] = [25, 'truck'];
+// additional elements can be any type from those previously specified.
--- LibraryManager/js/app.js
+++ LibraryManager/js/app.js
@@ -1,12 +1,19 @@
+"use strict";
+var lo = require("lodash");
 function GetAllBooks() {
     var books = [
-        { title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
-        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
-        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
-        { title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
+        { id: 1, title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+        { id: 2, title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { id: 3, title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+        { id: 4, title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
     ];
     return books;
 }
+var x = "From Here and Forward, and more.";
+var y = lo.snakeCase(x);
+var z = lo.camelCase(y);
+console.log('snake cased: ' + y);
+console.log('camel cased: ' + z);
 function LogFirstAvailable(books) {
     var numberOfBooks = books.length;
     var firstAvailable = '';
@@ -21,7 +28,7 @@ function LogFirstAvailable(books) {
     console.log('First available now how: ' + firstAvailable);
 }
 function getTitleByCategory(catFilter) {
-    console.log('Filtering by category: ' + catFilter);
+    console.log('Filtering by category: ' + Category[catFilter]);
     var allBooks = GetAllBooks();
     var filteredTitles = [];
     for (var _i = 0, allBooks_1 = allBooks; _i < allBooks_1.length; _i++) {
@@ -35,6 +42,8 @@ function getTitleByCategory(catFilter) {
     // });
     return filteredTitles;
 }
+//function getBookById
+// ts platform will assign 0 based index to each string value
 var Category;
 (function (Category) {
     Category[Category["Biography"] = 0] = "Biography";
@@ -42,10 +51,44 @@ var Category;
     Category[Category["Poetry"] = 2] = "Poetry";
 })(Category || (Category = {}));
 ;
+var BigCategory;
+(function (BigCategory) {
+    BigCategory[BigCategory["Biography"] = 1] = "Biography";
+    BigCategory[BigCategory["Fiction"] = 2] = "Fiction";
+    BigCategory[BigCategory["Poetry"] = 9] = "Poetry";
+})(BigCategory || (BigCategory = {}));
+; // 1, 2, 9
+var Category3;
+(function (Category3) {
+    Category3[Category3["Biography"] = 1] = "Biography";
+    Category3[Category3["Fiction"] = 2] = "Fiction";
+    Category3[Category3["Poetry"] = 3] = "Poetry";
+})(Category3 || (Category3 = {}));
+; // 1,2,3
 var allBooks = GetAllBooks();
 LogFirstAvailable(allBooks);
 function LogBookTitles(titles) {
-    titles.forEach(function () {
+    titles.forEach(function (title) {
+        console.log('Book Title Listing: ' + title + '.');
     });
 }
+function logCategory(cat) {
+    console.log('-- Index of fiction: ' + cat);
+}
+logCategory(BigCategory.Fiction);
+//*********************************************************
+LogBookTitles(getTitleByCategory(Category.Poetry));
+var fictionalBooks = getTitleByCategory(Category.Fiction);
+fictionalBooks.forEach(
+// val: current element, idx: index of the current element, arr current array
+function (val, idx, arr) {
+    console.log('>>>-- For each print: idx - ' + idx + ', value - ' + val);
+});
+// ways to declare arrays
+var strArray1 = ['hear', 'now', 'then'];
+var strArray2 = ['hear', 'now', 'then'];
+var anyArray = ['hear', 'now', 'then'];
+// tuples
+var myTuple = [25, 'truck'];
+// additional elements can be any type from those previously specified. 
 //# sourceMappingURL=app.js.map
